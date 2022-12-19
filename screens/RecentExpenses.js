018ad53de490a3c12302a853f5e7a5d41a1b040d
Sara Baqla <missatrox44@gmail.com>
2022-12-19 16:24:40 -0600

    added backend GET request

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,11 +1,21 @@
 
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import ExpensesOutput from '../components/expensesOutput/ExpensesOutput'
 import { ExpensesContext } from '../store/expenses-context'
 import { getDateMinusDays } from '../util/date';
+import { fetchExpenses } from '../util/http';
 
 export default function RecentExpenses() {
   const expensesCtx = useContext(ExpensesContext);
+
+  //useEffect should NOT return a promise
+  //nest function into async function to work around that
+  useEffect(() => {
+    async function getExpenses() {
+     const expenses =  await fetchExpenses();
+    }
+    getExpenses();
+  }, [])
 
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();
@@ -16,10 +26,10 @@ export default function RecentExpenses() {
 
   })
   return (
-    <ExpensesOutput 
-    expenses={recentExpenses} 
-    expensesPeriod='Last 7 Days' 
-    fallbackText='Saving up for that Kendrick Tour eh?'/>
+    <ExpensesOutput
+      expenses={recentExpenses}
+      expensesPeriod='Last 7 Days'
+      fallbackText='Saving up for that Kendrick Tour eh?' />
   )
 }
 
--- util/http.js
+++ util/http.js
@@ -1,11 +1,35 @@
 import axios from "axios";
 
+const BACKEND_URL = 'https://react-native-expenses-7dd1c-default-rtdb.firebaseio.com'
+
 //create helper functions that help with sending requests
 export function storeExpense(expenseData) {
   //url from firebase database
-  axios.post('https://react-native-expenses-7dd1c-default-rtdb.firebaseio.com/expenses.json',
-  expenseData);
+  axios.post(BACKEND_URL + '/expenses.json',
+    expenseData);
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(BACKEND_URL + '/expenses.json');
+  //next line will execute once response is there
+  //transform data from firebase into an array of objects that have the format we want to have
+  const expenses = [];
+  //console.log UNTRANSFORMED data:
+  // console.log(response.data) 
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description
+    };
+    expenses.push(expenseObj);
+  }
+  return expenses;
+
 }
 
 
 //post request to make new data
+
+//post and gets return promises
